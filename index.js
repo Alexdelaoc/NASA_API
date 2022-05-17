5cb86635b3552ba52080ef19a93bab70e1762a46
Alexdelaoc <alexdelaoc@gmail.com>
2022-05-17 18:30:20 +0200

    fixed index.js in backend

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ app.use(cors());
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 // Enrutador:
 const router = require('./routes/routes');
 
